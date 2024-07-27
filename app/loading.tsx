b520fbeab35d9b4bbbce7e549c12b08b0bd4506b
Reza Azarnia <rezaazarnia158@gmail.com>
2024-07-27 14:05:47 +0330

    feat: added loading indicator

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loader() {
+  return (
+    <div className="fixed top-0 left-0 bg-white w-full h-full z-40 flex flex-col justify-center items-center">
+      <div className=" spinner"></div>
+    </div>
+  );
+}
